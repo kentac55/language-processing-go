54fd591fa1b376230a754a0c5d14834c8f6912f3
kentac55 <dev.kentac55@gmail.com>
2017-05-28 02:12:00 +0900

    add P03

---
+++ src/P03_test.go
@@ -0,0 +1,14 @@
+package exercise
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestP03(t *testing.T) {
+	const i = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+	o := []int{3, 1, 4, 1, 6, 9, 2, 7, 5, 3, 5, 8, 9, 7, 9}
+	if x := P03(i); reflect.DeepEqual(x, o) {
+		t.Errorf("P03(%v) = %v, want %v", i, x, o)
+	}
+}
