cc4397629600cbae1c30d5ce3e56f490ac12f58b
kentac55 <dev.kentac55@gmail.com>
2017-05-27 23:59:20 +0900

    fix filename

--- P01_test.go
+++ P01_test.go
@@ -4,8 +4,8 @@ import (
 	"testing"
 )
 
-func TestP01(t *testing.T) {
-	const i, o = "stressed", "desserts"
+func TestP02(t *testing.T) {
+	const i, o = "パタトクカシーー", "パトカー"
 	if x := P01(i); x != o {
 		t.Errorf("P01(%v) = %v, want %v", i, x, o)
 	}
