6c4155e7a38d1698a3ebb9a27334f6bf09d3de89
kentac55 <dev.kentac55@gmail.com>
2017-05-27 23:38:22 +0900

    add P01

---
+++ P01_test.go
@@ -0,0 +1,12 @@
+package language_processing_go
+
+import (
+	"testing"
+)
+
+func TestP01(t *testing.T) {
+	const i, o = "stressed", "desserts"
+	if x := P01(i); x != o {
+		t.Errorf("P01(%v) = %v, want %v", i, x, o)
+	}
+}
