78547deb90200784bedeb41eb0dbe98479da7d0c
kentac55 <dev.kentac55@gmail.com>
2017-07-07 21:14:22 +0900

    fix P04

--- src/P04.go
+++ src/P04.go
@@ -3,16 +3,14 @@ package exercise
 import "strings"
 
 func P04(s string) map[string]int {
-	res := make(map[string]int)
-	order := []int{1, 5, 6, 7, 8, 9, 15, 16, 19}
+	res := make(map[string]int, 20)
+	//order := []int{1, 5, 6, 7, 8, 9, 15, 16, 19}
 	for i, v := range strings.Split(s, " ") {
 		i++
-		for _, o := range order {
-			if i == o {
-				res[v[0:1]] = i
-			} else {
-				res[v[0:2]] = i
-			}
+		if i == 1 || i == 5 || i == 6 || i == 7 || i == 8 || i == 9 || i == 15 || i == 16 || i == 19 {
+			res[v[0:1]] = i
+		} else {
+			res[v[0:2]] = i
 		}
 	}
 	return res
