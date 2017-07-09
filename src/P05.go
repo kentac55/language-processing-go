5cd5da0c9caf4b15d3e5719e53dc70e521cbc899
kentac55 <dev.kentac55@gmail.com>
2017-07-10 00:27:14 +0900

    fix P05

--- src/P05.go
+++ src/P05.go
@@ -3,34 +3,25 @@ package exercise
 import "strings"
 
 type biGram struct {
-	Word   []wordBiGram
-	Letter []letterBiGram
+	Word   [][]string
+	Letter []string
 }
 
-type wordBiGram struct {
-	x string
-	y string
-}
-
-type letterBiGram struct {
-	v string
-}
-
-func P05(s string) biGram {
+func P05(s string) *biGram {
 	slice := strings.Split(s, " ")
-	words := []wordBiGram{}
-	letters := []letterBiGram{}
+	words := [][]string{}
+	letters := []string{}
 	for i, v := range slice {
 		if i >= len(slice)-1 {
 			break
 		}
-		words = append(words, wordBiGram{v, slice[i+1]})
+		words = append(words, []string{v, slice[i+1]})
 	}
 	for i := range s {
 		if i >= len(s)-1 {
 			break
 		}
-		letters = append(letters, letterBiGram{s[i : i+2]})
+		letters = append(letters, s[i:i+2])
 	}
-	return biGram{words, letters}
+	return &biGram{words, letters}
 }
