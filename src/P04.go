4991b84612f63aff29cbca85949b1aa00a490f16
kentac55 <dev.kentac55@gmail.com>
2017-07-12 00:10:02 +0900

    remove comment

--- src/P04.go
+++ src/P04.go
@@ -4,7 +4,6 @@ import "strings"
 
 func P04(s string) map[string]int {
 	res := make(map[string]int, 20)
-	//order := []int{1, 5, 6, 7, 8, 9, 15, 16, 19}
 	for i, v := range strings.Split(s, " ") {
 		i++
 		if i == 1 || i == 5 || i == 6 || i == 7 || i == 8 || i == 9 || i == 15 || i == 16 || i == 19 {
