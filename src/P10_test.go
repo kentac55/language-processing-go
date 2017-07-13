636d4e3940d076c557cd2236ad243005cc19daf7
kentac55 <dev.kentac55@gmail.com>
2017-07-14 00:13:28 +0900

    fix test dependencies

--- src/P10_test.go
+++ src/P10_test.go
@@ -1,6 +1,10 @@
 package exercise
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -8,13 +12,28 @@ import (
 )
 
 func TestP10(t *testing.T) {
-	const path = "../hightemp.txt"
-	out, err1 := exec.Command("wc", "-l", path).Output()
-	if err1 != nil {
+	const path = "/tmp/hightemp.txt"
+	const url = "http://www.cl.ecei.tohoku.ac.jp/nlp100/data/hightemp.txt"
+	rs, err := http.Get(url)
+	if err != nil {
+		fmt.Errorf("cannot connet target")
+	}
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		fmt.Errorf("cannot read response body")
+	}
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Errorf("cannot open file")
+	}
+	defer file.Close()
+	file.Write(body)
+	out, err := exec.Command("wc", "-l", path).Output()
+	if err != nil {
 		t.Errorf("failure to exec command")
 	}
-	o, err2 := strconv.Atoi(strings.Split(string(out), " ")[0])
-	if err2 != nil {
+	o, err := strconv.Atoi(strings.Split(string(out), " ")[0])
+	if err != nil {
 		t.Errorf("failure atoi")
 	}
 	x := P10(path)
